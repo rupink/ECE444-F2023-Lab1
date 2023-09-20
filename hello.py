9a46945d6d8e28787bca2db044d5175493688654
rupink <rupin_23@hotmail.com>
2023-09-19 21:07:04 -0400

    Step 1 completed

--- hello.py
+++ hello.py
@@ -1,18 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    #uoft = StringField('What is your UofT Email Address?', validators=[Email(), DataRequired()])
+    submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 
 def index():
-
-
-    return(render_template('index.html',
-                        current_time=datetime.utcnow()))
+    name = None
+    #uoft = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        #name = form.name.data
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'))
+'''
+        #uoft = form.uoft.data
+        form.name.data = ''
+        #form.uoft.data = ''
+    #return render_template('index.html', form=form, name=name)#, uoft = uoft)'''
 
 @app.route('/user/<name>')
 def user(name):
