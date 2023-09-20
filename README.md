ea5dc64dfa6f87aab6b59283fc3835c9a143f832
rupink <rupin_23@hotmail.com>
2023-09-19 21:23:19 -0400

    Step 2 completed

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 
 # A3
 ![Alt text](image-3.png)
+
+# A4
+![Alt text](image-5.png)
+![Alt text](image-4.png)
+![Alt text](image-6.png)
+![Alt text](image-7.png)
--- hello.py
+++ hello.py
@@ -3,12 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    #uoft = StringField('What is your UofT Email Address?', validators=[Email(), DataRequired()])
+    uoft = EmailField('What is your UofT Email Address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -21,17 +21,21 @@ moment = Moment(app)
 
 def index():
     name = None
-    #uoft = None
+    uoft = None
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_uoft = session.get('uoft')
         #name = form.name.data
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_uoft is not None and old_uoft != form.uoft.data:
+            flash('Looks like you have changed your Email!')
         session['name'] = form.name.data
+        session['uoft'] = form.uoft.data
         return redirect(url_for('index'))
     return render_template('index.html',
-        form = form, name = session.get('name'))
+        form = form, name = session.get('name'), uoft = session.get('uoft'))
 '''
         #uoft = form.uoft.data
         form.name.data = ''
@@ -40,4 +44,4 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return ('<h1> Helloo , {}! </h1>'.format(name))
+    return ('<h1> Hello , {}! </h1>'.format(name))
